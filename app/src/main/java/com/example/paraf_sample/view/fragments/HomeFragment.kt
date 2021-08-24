14347fea369ea069b75f05000c55a42a8dd6573e
Sina <36819672+cnankh@users.noreply.github.com>
2021-08-24 14:44:05 +0430

    feat: setup project architecture (MVVM)

---
+++ app/src/main/java/com/example/paraf_sample/view/fragments/HomeFragment.kt
@@ -0,0 +1,23 @@
+package com.example.paraf_sample.view.fragments
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.paraf_sample.R
+import com.example.paraf_sample.viewmodel.HomeViewModel
+
+class HomeFragment : Fragment() {
+
+    private lateinit var viewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.home_fragment, container, false)
+    }
+    
+}
---
+++ app/src/main/java/com/example/paraf_sample/viewmodel/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.paraf_sample.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
